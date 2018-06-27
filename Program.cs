6cb8dd7c6b515cf415aa31aefd07b8a1e62b0448
shocklaing <shockwater@gmail.com>
2018-06-27 22:40:21 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_app_rest_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
